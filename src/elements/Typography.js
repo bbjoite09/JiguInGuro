31f73d9b79c3d91644716d05f46e800f0026002f
Heeeon <bbjoite09@naver.com>
2022-04-08 15:06:30 +0900

    Feat : 공용 텍스트 컴포넌트 생성

---
+++ src/elements/Typography.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Typography = props => {
+  // eslint-disable-next-line react/prop-types
+  const { type, size, children, color, margin, textAlign } = props;
+  return (
+    <p
+      style={{
+        textAlign,
+        fontFamily: type,
+        fontSize: size,
+        color: color,
+        margin,
+      }}>
+      {children}
+    </p>
+  );
+};
+
+Typography.defaultProps = {
+  children: null,
+  size: '15px',
+  type: 'Cafe24Ssurround',
+  color: '#078D68',
+  margin: false,
+};
+
+export default Typography;
