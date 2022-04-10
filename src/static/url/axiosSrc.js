42a176eaf46a606f7cee4a56841737e2e5c9280a
Heeeon <bbjoite09@naver.com>
2022-04-10 15:58:50 +0900

    Feat : backend 세팅

---
+++ src/static/url/axiosSrc.js
@@ -0,0 +1,7 @@
+const src = 'https://hardcarrygr.herokuapp.com/hardcarry/';
+export const axiosSrc = {
+  getComment: src + 'listCmnt',
+  postComment: src + 'addCmnt',
+  updateComment: src + 'updateCmnt',
+  deleteComment: src + 'deleteCmnt',
+};
