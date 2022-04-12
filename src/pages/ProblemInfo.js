296db0e17d1834bbf8f781c0b65192f945a18f72
Heeeon <bbjoite09@naver.com>
2022-04-13 01:48:27 +0900

    Feat : axios 프로미스 문제, slider 배치

--- src/pages/ProblemInfo.js
+++ src/pages/ProblemInfo.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled, { keyframes } from 'styled-components';
 import Typography from '../elements/Typography';
 import Slider from 'react-slick';
@@ -21,8 +21,10 @@ const ProblemInfo = () => {
     arrows: false,
     centerPadding: '0px',
   };
-
-  const data = service.comment.getComment();
+  const [data, setData] = useState();
+  useEffect(() => {
+    service.comment.getComment().then(res => setData(res));
+  }, []);
   console.log(data);
 
   return (
@@ -45,73 +47,82 @@ const ProblemInfo = () => {
         </Typography>
         <img src={divider1} style={{ width: '55%', margin: ' 5% 0 15% 10%' }} />
       </div>
+
       <Slider {...settings}>
-        <div style={{ width: '90%', padding: 0, margin: 0 }}>
-          <Card>
-            <img src={one} style={{ width: '10%', marginTop: '9%' }} />
-            <Typography color="#4D99CB" size="2.5rem">
-              2025년 <span style={{ color: '#EADB50' }}>수도권</span>
-              <br />
-              쓰레기 매립지 폐쇄 <span style={{ color: '#EADB50' }}>예정</span>
-            </Typography>
-            <Typography
-              type="AppleSDGothicNeoB00"
-              color="#4D99CB"
-              size="1.7rem"
-              margin="9em 0 0 0">
-              앞으로 3년 후
-            </Typography>
-          </Card>
-        </div>
         <>
-          <Card>
-            <img src={two} style={{ width: '10%', marginTop: '9%' }} />
-            <Typography color="#4D99CB" size="2.5rem">
-              구로구 쓰레기 배출량<span style={{ color: '#EADB50' }}>은</span>
-              <br />
-              서울<span style={{ color: '#EADB50' }}>에서도</span> Top 5
-            </Typography>
-            <Typography
-              type="AppleSDGothicNeoB00"
-              color="#4D99CB"
-              size="1rem"
-              margin="-1.8rem">
-              출처: 수도권매립지관리공사 홈페이지, 2021 기준
-            </Typography>
-            <Typography
-              type="AppleSDGothicNeoB00"
-              color="#4D99CB"
-              size="1.7rem"
-              margin="10em 0 0 0">
-              서울 자치구 중 5위
-            </Typography>
-          </Card>
+          <div style={{ padding: '0 1em 0 5em' }}>
+            <Card>
+              <img src={one} style={{ width: '10%', marginTop: '9%' }} />
+              <Typography color="#4D99CB" size="2.5rem">
+                2025년 <span style={{ color: '#EADB50' }}>수도권</span>
+                <br />
+                쓰레기 매립지 폐쇄{' '}
+                <span style={{ color: '#EADB50' }}>예정</span>
+              </Typography>
+              <img />
+              <Typography
+                type="AppleSDGothicNeoB00"
+                color="#4D99CB"
+                size="1.7rem"
+                margin="9em 0 0 0">
+                앞으로 3년 후
+              </Typography>
+            </Card>
+          </div>
         </>
         <>
-          <Card>
-            <img src={three} style={{ width: '10%', marginTop: '9%' }} />
-            <Typography color="#4D99CB" size="2.5rem">
-              <span style={{ color: '#EADB50' }}>나혼자 산다?</span>
-              <br />
-              쓰레기와 함께 산다!
-            </Typography>
-            <Typography
-              type="AppleSDGothicNeoB00"
-              color="#4D99CB"
-              size="1rem"
-              margin="-1.8rem">
-              출처: 서울디지털재단 발행 보고서, 2020 발행
-            </Typography>
-            <Typography
-              type="AppleSDGothicNeoB00"
-              color="#4D99CB"
-              size="1.7rem"
-              margin="9em 0 0 0">
-              1인가구 일회용 쓰레기 배출량
-              <br />
-              다인가구의 2.3배
-            </Typography>
-          </Card>
+          <div style={{ padding: '0 1em 0 5em' }}>
+            <Card>
+              <img src={two} style={{ width: '10%', marginTop: '9%' }} />
+              <Typography color="#4D99CB" size="2.5rem">
+                구로구 쓰레기 배출량<span style={{ color: '#EADB50' }}>은</span>
+                <br />
+                서울<span style={{ color: '#EADB50' }}>에서도</span> Top 5
+              </Typography>
+              <Typography
+                type="AppleSDGothicNeoB00"
+                color="#4D99CB"
+                size="1rem"
+                margin="-1.8rem">
+                출처: 수도권매립지관리공사 홈페이지, 2021 기준
+              </Typography>
+              <Typography
+                type="AppleSDGothicNeoB00"
+                color="#4D99CB"
+                size="1.7rem"
+                margin="10em 0 0 0">
+                서울 자치구 중 5위
+              </Typography>
+            </Card>
+          </div>
+        </>
+        <>
+          <div style={{ padding: '0 1em 0 5em' }}>
+            <Card>
+              <img src={three} style={{ width: '10%', marginTop: '9%' }} />
+              <Typography color="#4D99CB" size="2.5rem">
+                <span style={{ color: '#EADB50' }}>나혼자 산다?</span>
+                <br />
+                쓰레기와 함께 산다!
+              </Typography>
+              <Typography
+                type="AppleSDGothicNeoB00"
+                color="#4D99CB"
+                size="1rem"
+                margin="-1.8rem">
+                출처: 서울디지털재단 발행 보고서, 2020 발행
+              </Typography>
+              <Typography
+                type="AppleSDGothicNeoB00"
+                color="#4D99CB"
+                size="1.7rem"
+                margin="9em 0 0 0">
+                1인가구 일회용 쓰레기 배출량
+                <br />
+                다인가구의 2.3배
+              </Typography>
+            </Card>
+          </div>
         </>
       </Slider>
       <div
@@ -181,7 +192,7 @@ const Container = styled.div`
 const Card = styled.div`
   background-color: white;
   border-radius: 20px;
-  width: 85%;
+  width: 110%;
   height: 55vh;
   margin: 0 auto;
   display: flex;
