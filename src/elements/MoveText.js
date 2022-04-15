397e7672e2aa996a185d45ec948402fccbbe753e
Heeeon <bbjoite09@naver.com>
2022-04-15 23:49:03 +0900

    Feat : 움직이는 텍스트 컴포넌트 분리

---
+++ src/elements/MoveText.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+const MoveText = props => {
+  // eslint-disable-next-line react/prop-types
+  const { backgroundColor, color } = props;
+  return (
+    <div
+      style={{
+        backgroundColor,
+        width: '100%',
+        height: '2.5em',
+        lineHeight: '2.5em',
+        overflow: 'hidden',
+        whiteSpace: 'nowrap',
+        zIndex: 2,
+      }}>
+      <AnimationText style={{ color }}>
+        #제로인구로 #제로in구로 #제로인구로 #제로in구로 #제로인구로
+      </AnimationText>
+    </div>
+  );
+};
+
+MoveText.defaultProps = {
+  backgroundColor: 'white',
+  color: '#078d68',
+};
+
+const move = keyframes`{
+  from {
+    margin-left: 100%;
+  }
+  to {
+    margin-left: -150%;
+  }
+}`;
+
+const AnimationText = styled.p`
+  margin: 0;
+  font-family: Cafe24Ssurround;
+  font-size: 1.4rem;
+  animation: ${move} 5s linear infinite;
+`;
+
+export default MoveText;
