6018f4d9faf284d12a7c4d635ae730c2efb2b99e
Heeeon <bbjoite09@naver.com>
2022-05-10 00:16:14 +0900

    Feat : modal 영역 외 터치

--- src/components/main/Comment.js
+++ src/components/main/Comment.js
@@ -34,9 +34,22 @@ const Comment = () => {
           left: 0,
           bottom: 0,
           right: 0,
-          background: 'rgba(0, 0, 0, 0.8)',
           zIndex: 5,
+          width: 'auto',
+          height: 'auto',
         }}>
+        <div
+          style={{
+            position: 'fixed',
+            width: '100%',
+            height: '100%',
+            top: 0,
+            left: 0,
+            background: 'rgba(0, 0, 0, 0.8)',
+            zIndex: 5,
+          }}
+          onClick={() => setModal(false)}
+        />
         <div
           style={{
             width: window.innerWidth <= '500' ? '80%' : '400px',
